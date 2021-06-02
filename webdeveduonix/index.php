7deae3445a0772de83489b90ac4b3e372f118913
Madár Bálint <balint.mb@gmail.com>
2021-06-02 14:11:26 +0200

    Structure of the project

---
+++ webdeveduonix/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <p> Linkek<p>
+   </br>
+
+    <?php
+echo "Here are our files";
+echo "<br>";
+$path = ".";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='$path/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?>
+</body>
+</html>
